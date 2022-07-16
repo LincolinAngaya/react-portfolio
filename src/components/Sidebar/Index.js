616adfefb0de8f24cc06195f6c0039433a9daad6
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-16 23:08:49 +0300

    add social media icons

--- src/components/Sidebar/Index.js
+++ src/components/Sidebar/Index.js
@@ -1,6 +1,15 @@
 import './index.scss'
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faGithub,
+    faTwitter,
+    faInstagram,
+    faFacebook
+  } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar= () => {
@@ -10,6 +19,45 @@ const Sidebar= () => {
      <Link className ='logo' to='/'>
         <img src={logo}  alt='logo'/>
      </Link>
+
+     <nav>
+        <NavLink exact='true' activeclassname="active" to="/">
+               <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact='true' activeclassname="active" className="about-link" to="/about">
+               <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact='true' activeclassname="active" className="contact-link" to="/contact">
+               <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+     </nav>
+     <ul>
+        <li>
+       <a target="_blank" rel="noreferrer" href="">
+       <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+       </a>
+        </li>
+        <li>
+       <a target="_blank" rel="noreferrer" href="">
+       <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+       </a>
+        </li>
+        <li>
+       <a target="_blank" rel="noreferrer" href="">
+       <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+       </a>
+        </li>
+        <li>
+       <a target="_blank" rel="noreferrer" href="">
+       <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+       </a>
+        </li>
+        <li>
+       <a target="_blank" rel="noreferrer" href="">
+       <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+       </a>
+        </li>
+     </ul>
     </div>
   )
 }
