d9e1336c8ba66d88a3feee1b48346a9de397a22e
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-17 00:05:33 +0300

    outlet is working

--- src/components/Layout/Index.js
+++ src/components/Layout/Index.js
@@ -1,13 +1,20 @@
 import './index.scss'
+import { Outlet } from 'react-router-dom'
 import React from 'react'
 import Sidebar from '../Sidebar/Index'
 
 const Layout = () => {
   return (
-    <div>
-      
-      hello
+    <div className='App'>
       <Sidebar />
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
+      <Outlet />
+        <span className='tags bottom-tags'>&lt;/body&gt;
+        <br />
+        <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }
