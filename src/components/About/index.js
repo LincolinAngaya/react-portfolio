b1c7e12b2e50e125a7db920e017fff282784f231
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-17 13:35:07 +0300

    chsnge text content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -25,15 +25,6 @@ const About = () => {
           <h1>
            About Me
           </h1>
-          <p>
-            I'm very ambitious front-end developer looking for a role in
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
-          </p>
-          <p align="LEFT">
-            I'm quietly confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
-          </p>
           <p>My name is <b>Lincolin Angaya</b>.I am a <b>Certified </b> 
           Web Developer from Moringa School of Software Engineering and I'm based in 
           <b>Nairobi Kenya</b>.<br />My Main focus has mainly been on front-end 
@@ -41,13 +32,23 @@ const About = () => {
           clean code and perfect design. I also make the website more & more interactive 
           with web animations.A responsive <br />design makes your website accessible 
           to all users, regardless of their device.
-            </p>
+          </p>
+          <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          {/* <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p> */}
+         
           <p>
             If I need to define myself in one sentence that would be a family
             person, father of a beautiful daughter, a sports fanatic,
             photography enthusiast, and tech-obsessed!!!
           </p>
-          <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+          <Link to="/contact" className='flat-button'>DOWNLOAD MY CV</Link>
         </div>
 
         <div className="stage-cube-cont">
