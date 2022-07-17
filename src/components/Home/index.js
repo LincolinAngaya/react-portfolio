784fdf1ebea04d6578e4cf4408341acff5e554ef
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-17 12:19:08 +0300

    add logo folder

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
+
+import { useEffect, useState } from 'react'
 import './index.scss'
 import { Link} from 'react-router-dom';
+
 const Home = () => {
+
   return (
     <>
     <div className='container home-page'>
         <div className='text-zone'>
-            <h1>Hi, <br /> I'm
+            <h1>Hi, <br /> I'm Lincolin Angaya
+
             <br />
-            Web Developer
+            A Web Developer
             </h1>
-            <h2>Frontend Developer /Javascript Expert / Youtuber</h2>
-            <Link to="/contact" className='flat-button'>CONTACT</Link>
+            <h2>Frontend  & Backend Developer|UX/UI Designer</h2>
+            <h2>Graphic Designer |I.T Support |ICT Officer</h2>
+            <Link to="/contact" className='flat-button'>CONTACT ME</Link>
         </div>
     </div>
     
